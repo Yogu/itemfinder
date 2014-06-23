563b0eca5e0d403bf9d7ae3371e4e0d1446c47ea
Jan Melcher <info@yogularm.de>
2014-06-23 23:24:55 +0200

    Removed CLI app

--- src/main/java/de/yogularm/minecraft/itemfinder/App.java
+++ src/main/java/de/yogularm/minecraft/itemfinder/App.java
@@ -6,7 +6,6 @@ import de.yogularm.minecraft.itemfinder.gui.GUI;
 
 public class App {
 	public static void main(String[] args) throws IOException {
-		//ItemFinderCli.main(args);
 		GUI gui = new GUI();
 		gui.show();
 	}
